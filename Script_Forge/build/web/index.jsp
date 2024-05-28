92981ace8bb30ed640b8e6e567a62252f66c0d83
Lua_Sky__ <luaskyyym00n@gmail.com>
2024-05-27 22:36:23 -0300

    updated gpt related files
    updated gpt related files alloying it to work properly, still need to update it to make it look like what is desired to look like

--- Script_Forge/build/web/index.jsp
+++ Script_Forge/build/web/index.jsp
@@ -10,24 +10,49 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Script Forge</title>
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
         <%@include file="WEB-INF/jspf/navbar.jspf" %>
         <%@include file="WEB-INF/jspf/head-libs.jspf" %>
         <%@include file="WEB-INF/jspf/body-libs.jspf" %>
         
-        <div style="margin-top: 100px; margin-left: 2%; margin-right: 2%;">
-            <h1>ChatGPT 4 API Test</h1>
+        <main style="margin-top: 100px; margin-left: 2%; margin-right: 2%;">
+            <header>
+                <h1>ChatGPT Clone - Model:</h1>
+                <select id="models">
+                    <option value="gpt-4-1106-preview">gpt-4-1106-preview</option>
+                     <option value="gpt-4">gpt-4</option>
+                    <option value="gpt-3.5-turbo-1106" selected>gpt-3.5-turbo-1106</option>
+                    <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+                     <!-- <option value="gpt-3.5-turbo-instruct">gpt-3.5-turbo-instruct</option>
+                    <option value="text-davinci-003">text-davinci-003</option> -->
+                </select>
+             </header>
+            
+            <section id="messages"></section>
+            <form>
+                <input type="text" name="message" id="message" placeholder="Message ChatGPT..."/>
+                
+                <button type="button" id="send-message" class="color-gray" disabled>
+                    <i class="bi bi-arrow-up-square-fill"></i>
+                </button>
+            </form>
+        </main>
+        <script src="script.js"></script>
+    </body>
+</html>
+<!--
+<h1>ChatGPT 4 API Test</h1>
             <form>
                 <div class="mb-3">
                     <label for="mytext" class="form-label">Insira uma descrição de como você quer que seja o jogo:</label>
                         <input class="form-control" type="text" placeholder="Digite Aqui." aria-label="default input example" rows="20" style="width: 70%;">                  
-                        <textarea class="form-control"id="exampleFormControlTextarea1" rows="4" style="width: 70%;"></textarea>
+                        <textarea class="form-control"id="exampleFormControlTextarea1" rows="4" style="width: 70%;" readonly></textarea>
                     <div id="emailHelp" class="form-text">algo em baixo para alterar.</div>
                 </div>
                 <button type="submit" class="btn btn-primary mb-3">Submit</button>
             </form>
             <script src="script.js"></script>
-        </div>
-    </body>
-</html>
+!-->
+
--- Script_Forge/build/web/script.js
+++ Script_Forge/build/web/script.js
@@ -1,42 +1,113 @@
-const form = document.getElementById('chat-form');
-const mytextInput = document.getElementById('mytext');
-const responseTextarea = document.getElementById('response');
+const sectionMessages = document.querySelector("#messages");
+const inputMessage = document.querySelector("#message");
+const buttonMessage = document.querySelector("#send-message");
+const formMessage = document.querySelector("form");
+const model = "gpt-3.5-turbo-1106"
 
-const API_KEY = "";
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const mytext = mytextInput.value.trim();
+const BASE_URL = "https://api.openai.com/v1/chat/completions";
+const API_KEY = "..."
 
-    if (mytext) {
-        try {
-            const response = await fetch('https://api.openai.com/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${API_KEY}`,
-                },
-                body: JSON.stringify({
-                    model: 'gpt-3.5 turbo',
-                    messages: [{ role: 'user', content: mytext }],
-                    temperature: 0.7,
-                    top_p: 0.7,
-                    n: 1,
-                    stream: false,
-                    presence_penalty: 0,
-                    frequency_penalty: 0,
-                }),
-            });
 
-            if (response.ok) {
-                const data = await response.json();
-                responseTextarea.value = data.choices[0].message.content;
-            } else {
-                responseTextarea.value = 'Error: Unable to process your request.';
-            }
-        } catch (error) {
-            console.error(error);
-            responseTextarea.value = 'Error: Unable to process your request.';
-        }
-    }
+formMessage.addEventListener("submit", (e) => e.preventDefault());
+buttonMessage.addEventListener("click", insertMessageInHTML);
+
+inputMessage.addEventListener("keyup", (event) => {
+    const hasValue = inputMessage.value !== "";
+
+    buttonMessage.classList.toggle("color-white", hasValue);// talvez possa ser removido
+    buttonMessage.classList.toggle("color-gray", !hasValue);// talvez possa ser removido
+    buttonMessage.disabled = !hasValue;
+
+    if (hasValue && event.key === "Enter") {
+        event.preventDefault();
+        insertMessageInHTML();
+    } 
 });
+
+async function insertMessageInHTML() {
+    const userInputMessage = inputMessage.value;
+    
+     // deve ser alterado
+    sectionMessages.innerHTML =  `
+    <div class="image-name">
+        <i class="bi bi-person-circle"></i>
+        <h4>You</h4>
+        </div>
+    <p class="message-p"> ${userInputMessage} </p>
+    `;
+    
+    formMessage.reset();
+    resetButtonState();
+    
+    const responseGPT = await postMessageGPT(userInputMessage);
+    sectionMessages.innerHTML += `
+    <div class="image-name">
+      <img src="./icon.webp">
+      <h4>ChatGPT</h4>
+    </div>
+    <p class="message-p"> ${responseGPT} </p>
+  `;
+}
+
+function resetButtonState() {
+  buttonMessage.classList.remove("color-white"); // talvez possa ser alterado
+  buttonMessage.classList.add("color-gray");     // talvez possa ser alterado
+  buttonMessage.disabled = true;
+}
+
+async function postMessageGPT(message) {
+    const body = {
+        messages: [
+            {
+                content: message,
+                role: "system",
+            },
+        ],
+        model: model, // pode ser alterado
+        temperature: 0.2,
+        max_tokens: 2000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+    };
+    
+    showLoadingIndicator();
+    
+    try{
+        const response = await fetch(BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${API_KEY}`,
+            },
+            body: JSON.stringify(body),
+        })
+        
+        const returnResponseJson = await response.json();
+        
+        console.log(returnResponseJson);
+        return returnResponseJson.choices[0].message.content;
+  
+    }catch (error){
+        console.error("Erro na requisição:", error.message);
+        return error.message;
+    } finally {
+        hideLoadingIndicator();
+    }
+    
+    
+}
+
+function showLoadingIndicator() {
+  const loadingElement = document.createElement("div");
+  loadingElement.className = "lds-ellipsis";
+  loadingElement.innerHTML = "<div></div><div></div><div></div><div></div>";
+  sectionMessages.appendChild(loadingElement);
+}
+
+function hideLoadingIndicator() {
+  const loadingElement = sectionMessages.querySelector(".lds-ellipsis");
+  if (loadingElement) loadingElement.remove();
+}
+
